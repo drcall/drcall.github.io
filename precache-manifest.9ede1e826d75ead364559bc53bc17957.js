984af7faa8c47664a5032ba0de78da9b6e4ad114
Denis <drcall@users.noreply.github.com>
2020-08-19 16:26:11 -0400

    Updates

---
+++ precache-manifest.9ede1e826d75ead364559bc53bc17957.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737cd251a9ac2f1808461e812c0d6047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c78b7f342ad65896a89",
+    "url": "/static/css/main.29b55c2b.chunk.css"
+  },
+  {
+    "revision": "534dc640048571d0c468",
+    "url": "/static/js/2.99416586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99416586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c78b7f342ad65896a89",
+    "url": "/static/js/main.8fe13412.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8f7aa08605bc64d4bcee12a7fdf8f5c5",
+    "url": "/static/media/albm.8f7aa086.png"
+  },
+  {
+    "revision": "127d35352dd45cef904abaf26dc0f8f8",
+    "url": "/static/media/background.127d3535.jpg"
+  },
+  {
+    "revision": "14c8c6e5fa2af593ddb9baff4aa45680",
+    "url": "/static/media/c1.14c8c6e5.png"
+  },
+  {
+    "revision": "a44e6e43fdf8de211d7ac96c57819916",
+    "url": "/static/media/c2.a44e6e43.png"
+  },
+  {
+    "revision": "f2683c16e3bf587fae8e2d4fdcb88d44",
+    "url": "/static/media/c3.f2683c16.png"
+  },
+  {
+    "revision": "6c6298502e3e5421f546f943e0af3249",
+    "url": "/static/media/cf.6c629850.png"
+  },
+  {
+    "revision": "68080f05ebbd6a4f326e30199e1fb0e8",
+    "url": "/static/media/dashboard.68080f05.jpg"
+  },
+  {
+    "revision": "3e0a0e3c4a76cd4e545fe08ae7d12bae",
+    "url": "/static/media/drcall.3e0a0e3c.jpg"
+  },
+  {
+    "revision": "548ddd8c2b0b7220d2c9a39d6ef79c76",
+    "url": "/static/media/favicon.548ddd8c.ico"
+  },
+  {
+    "revision": "adfed61f716482576459a4decc2d5f60",
+    "url": "/static/media/game.adfed61f.jpg"
+  },
+  {
+    "revision": "1884ba46a727b311e69828107c76a283",
+    "url": "/static/media/health.1884ba46.png"
+  },
+  {
+    "revision": "e8af5d9bd2a572090b0401885a06a95f",
+    "url": "/static/media/pathfinding.e8af5d9b.png"
+  },
+  {
+    "revision": "7816cf215847d84aa5b7eed048c1cef1",
+    "url": "/static/media/tw.7816cf21.png"
+  },
+  {
+    "revision": "84a2083d94da46a622f71828272971d6",
+    "url": "/static/media/uva.84a2083d.png"
+  },
+  {
+    "revision": "8ced93376e8c6279dcf72971ba7da3c2",
+    "url": "/static/media/vertigo.8ced9337.jpg"
+  },
+  {
+    "revision": "df4987e74abe88434630c1d422635386",
+    "url": "/static/media/vertigo.df4987e7.png"
+  }
+]);
